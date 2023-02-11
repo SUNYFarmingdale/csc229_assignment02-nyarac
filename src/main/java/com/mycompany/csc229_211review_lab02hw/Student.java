2696d59681477a0fa3dc3ba50373b971e09e5082
nyarachoudhury <nyarachoudhury@nyaras-mbp>
2023-02-10 21:53:56 -0500

    homework 2 working version

--- src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -1,16 +1,47 @@
 package com.mycompany.csc229_211review_lab02hw;
 
-/**
- *
- * @author MoaathAlrajab
- */
-public class Student {
-	// ToDo 1: Make this class a child of Person
-	
-	// ToDo 2: Fix the resulting errors
-	
-	// ToDo 3: Add a field for GPA and create setter and getter
-	
-	// ToDo 4: Add comments to your code
+public class Student extends Person { //extends is the keyword used to make student a child class of person
+    
+    private double GPA; //GPA initialized here
+    
+    private String address;
+    
+    public Student(String name, short age) { //new constructor
+        super(name, age);
+        address = "";
+    }
+    
+    public Student(String name, short age, double GPA, String address) {
 
-}
+        super(name, age);
+
+        this.GPA = GPA;
+
+        this.address = address;
+
+    }
+
+   
+    @Override 
+    
+   public String getAddress() { //overriding the setter and getter for address so it can be used here
+       return address;
+   }
+    @Override
+    public void setAddress(String Address) {
+        this.address = address; 
+    }
+   
+    public double getGPA () { //getter for GPA
+        return GPA;
+    }
+    public void setGPA(double GPA) { //setter for GPA
+        this.GPA = GPA;
+    }
+    
+    @Override 
+    public String toString() { //the toString method needed to print the correct information for Student
+        return getName() + " is " + getAge() + " years old and has a " + getGPA() + " GPA" + " and lives in " + getAddress(); 
+    }
+    
+} 
